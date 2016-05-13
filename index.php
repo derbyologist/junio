86bc826eaf3559961508623e123be47b62af602a
Anonymous <the_arc_of_diablo@hotmail.co.jp>
2016-05-13 11:58:07 +0000

    RIOT導入

--- index.php
+++ index.php
@@ -1,28 +1,75 @@
-
-<!doctype html>
-
-<html>
+<html>  
   <head>
-    <meta charset="utf-8">
-    <title>Hello world!</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.2/normalize.min.css">
-    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+
+    <title>${title}</title>
+    <meta name="description" content="${description}" />
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/riot/2.3.18/riot+compiler.js'></script>
+
+<script type='riot/tag'>  
+  <app>
+    <h1>Event handlers</h1>
+
+    <button onclick='{click}'>click</button>
+
+    <ul>
+      <li each='{list}'>
+        <button onclick='{clickItem}'>{name}</button>
+      </li>
+    </ul>
+
+    this.list = [
+      {name:'hoge'},
+      {name:'foo'},
+      {name:'bar'},
+    ];
+
+    this.click = function() {
+      console.log('click されたよー');
+    };
+
+    this.clickItem = function(e) {
+      console.log(e.item.name + 'が click されたよー');
+    };
+    <h1>form</h1>
+
+    <form onsubmit='{submit}'>
+      <input type='text' name='username' />
+      <input type='password' name='password' />
+      <button type='submit' name='submit'>login</button>
+    </form>
+
+    this.on('mount', function() {
+      this.username.value = 'phi';
+      this.password.value = '1234abcd';
+    });
+
+    this.submit = function() {
+      console.log('username: ', this.username.value);
+      console.log('password: ', this.password.value);
+    };
+  </app>
+</script> 
+
+    <style>${style}</style>
   </head>
-
   <body>
+    <!-- ユーザー定義のタグをここに展開 -->
+    <app title='Hello, Riot.js'></app>
 
-    <app></app>
+    <!-- app タグを定義 -->
+    <script type="riot/tag">
+      <app>
+        <h1>{ title }</h1>
+        this.title = opts.title;
+      </app>
+    </script>
 
-    <script src="tag/app.html" type="riot/tag"></script>
-    <script src="tag/app-footer.html" type="riot/tag"></script>
-    <script src="tag/app-hero.html" type="riot/tag"></script>
-    <script src="tag/app-links.html" type="riot/tag"></script>
-    <script src="tag/app-menu.html" type="riot/tag"></script>
-    <script src="tag/ring.html" type="riot/tag"></script>
-    
-    <script src="https://cdn.jsdelivr.net/g/riot@2.0(riot.min.js+compiler.min.js)"></script>
-    <script>riot.mount('*')</script>
-
+    <script>
+      // マウント
+      riot.mount('*');
+    </script>
   </body>
-
-</html>
+</html>  
